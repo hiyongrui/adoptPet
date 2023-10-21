cab340e2961f49447caf146a6216419e48e914d0
gowhr <yongrui.tan.2022@scis.smu.edu.sg>
2023-10-21 18:11:34 +0800

    set up npm and prettier
    npm init -y, -y to skip the questions asked
    npm i -D prettier, -D = --save-dev, into devDependencies, -d will be dependencies
    prettier auto insert semicolons, format tabs when saved
    
    in settings.json
    - "editor.formatOnSave": true,
    - "prettier.requireConfig": true
    - "editor.defaultFormatter": "esbenp.prettier-vscode",

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 }; //Pet rubber stamp function, stamp it 3 time below
 
 // one-way data flow, from parent App to child Pet
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {}, // or null, attriute on the HTML element like {id: "my-id"}
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Luna",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                animal: "Bird",
-                name: "Pepper",
-                breed: "Cockatiel"
-            }),
-            React.createElement(Pet, {
-                animal: "Cat",
-                name: "Doink",
-                breed: "Breed"
-            }),
-        ]
-    )
+  return React.createElement(
+    "div",
+    {}, // or null, attriute on the HTML element like {id: "my-id"}
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        animal: "Dog",
+        name: "Luna",
+        breed: "Havanese",
+      }),
+      React.createElement(Pet, {
+        animal: "Bird",
+        name: "Pepper",
+        breed: "Cockatiel",
+      }),
+      React.createElement(Pet, {
+        animal: "Cat",
+        name: "Doink",
+        breed: "Breed",
+      }),
+    ]
+  );
 };
 
 const container = document.getElementById("root");
