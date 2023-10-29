f07012a5976fce5af92267b50d290a173e08e4c0
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-10-29 23:32:29 +0800

    useQuery() to cache
    ["details", id] pass as queryKey
    
    can use results.isLoading(), results.isError() etc

--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,8 +1,8 @@
 const fetchPet = async ({ queryKey }) => {
-  const id = queryKey[1];
+  const id = queryKey[1]; // ["details", id] will be passed to queryKey
 
-  const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets/${id}`);
-
+  const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
+    
   if (!apiRes.ok) {
     throw new Error(`details/${id} fetch not ok error`, apiRes.status);
   }
