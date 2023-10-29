dae785446ec330e19372a7e17ccd980dfd572070
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-10-29 19:54:30 +0800

    useEffect()
    request initial pet array from API on initial load of page
    empty array at end of useEffect() is where to declare data dependencies, react want to know when to run that effect again, [] to indicate never again. [animal] means everytime the animal dropdown is selected, run again
    form onSubmit(), e.preventDefault() prevent the page from reloading

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -6,11 +8,28 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = ["Poodle"];
+  const [pets, setPets] = useState([]);
+  const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -20,6 +39,7 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+
         <label htmlFor="animal">
           Animal
           <select
@@ -38,6 +58,7 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
+        
         <label htmlFor="breed">
           Breed
           <select
@@ -54,8 +75,18 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
+
         <button>Submit</button>
       </form>
+
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
